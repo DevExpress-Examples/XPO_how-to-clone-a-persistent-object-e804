0f0882c45f072c41a39f539775a8ff4d7f232bd8
DevExpressExampleBot <fakeEmail>
2022-11-17 09:22:44 +0400

    autogenerate VB

--- VB/SimpleObject/Form1.vb
+++ VB/SimpleObject/Form1.vb
@@ -1,9 +1,9 @@
-﻿Imports System
-Imports System.Windows.Forms
+Imports System
 Imports DevExpress.Xpo
 Imports DevExpress.Xpo.DB
 
 Namespace SimpleObject
+
     ''' <summary>
     ''' Summary description for Form1.
     ''' </summary>
@@ -11,11 +11,11 @@ Namespace SimpleObject
         Inherits Form
 
         Private listBox1 As ListBox
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
         Private ReadOnly components As System.ComponentModel.Container = Nothing
-
 
         Public Sub New()
             InitializeComponent()
@@ -27,60 +27,67 @@ Namespace SimpleObject
                     components.Dispose()
                 End If
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
-            Me.listBox1 = New System.Windows.Forms.ListBox()
+            listBox1 = New Windows.Forms.ListBox()
             Me.SuspendLayout()
             ' 
             ' listBox1
             ' 
-            Me.listBox1.Dock = System.Windows.Forms.DockStyle.Fill
-            Me.listBox1.Location = New System.Drawing.Point(0, 0)
-            Me.listBox1.Name = "listBox1"
-            Me.listBox1.Size = New System.Drawing.Size(413, 277)
-            Me.listBox1.TabIndex = 0
+            listBox1.Dock = Windows.Forms.DockStyle.Fill
+            listBox1.Location = New System.Drawing.Point(0, 0)
+            listBox1.Name = "listBox1"
+            listBox1.Size = New System.Drawing.Size(413, 277)
+            listBox1.TabIndex = 0
             ' 
             ' Form1
             ' 
             Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
             Me.ClientSize = New System.Drawing.Size(413, 277)
-            Me.Controls.Add(Me.listBox1)
+            Me.Controls.Add(listBox1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            Me.ResumeLayout(False)
-
+             ''' Cannot convert AssignmentExpressionSyntax, System.NullReferenceException: Object reference not set to an instance of an object.
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitAssignmentExpression(AssignmentExpressionSyntax node)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+''' 
+''' Input:
+'''             this.Load += new System.EventHandler(this.Form1_Load)
+'''  Me.ResumeLayout(False)
         End Sub
-        #End Region
 
+#End Region
         ''' <summary>
         ''' The main entry point for the application.
         ''' </summary>
-        <STAThread> _
+        <STAThread>
         Shared Sub Main()
             XpoDefault.DataLayer = New SimpleDataLayer(New InMemoryDataStore())
             Application.Run(New Form1())
-
         End Sub
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-            Dim uow1 As New UnitOfWork()
-            Dim obj As New Parent(uow1) With {.Name = "Parent A"}
-            Dim ch1 As New Child(uow1) With {.Name = "Child 1"}
-            Dim ch2 As New Child(uow1) With {.Name = "Child 2"}
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            Dim uow1 As UnitOfWork = New UnitOfWork()
+            Dim obj As Parent = New Parent(uow1) With {.Name = "Parent A"}
+            Dim ch1 As Child = New Child(uow1) With {.Name = "Child 1"}
+            Dim ch2 As Child = New Child(uow1) With {.Name = "Child 2"}
             obj.Children.Add(ch1)
             obj.Children.Add(ch2)
             obj.Save()
             uow1.CommitChanges()
             listBox1.Items.Add(String.Format("Object created: {0}", obj.Name))
-            Dim uow2 As New UnitOfWork()
-            Dim cloneHelper As New CloneHelper(uow2)
-            Dim clone As Parent = cloneHelper.Clone(Of Parent)(obj)
+            Dim uow2 As UnitOfWork = New UnitOfWork()
+            Dim cloneHelper As CloneHelper = New CloneHelper(uow2)
+            Dim clone As Parent = cloneHelper.Clone(obj)
             listBox1.Items.Add(String.Format("Object cloned: {0}", clone.Name))
             listBox1.Items.Add(String.Format("Object's child 1: {0}", clone.Children(0).Name))
             listBox1.Items.Add(String.Format("Object's child 2: {0}", clone.Children(1).Name))
--- VB/SimpleObject/ObjectClass.vb
+++ VB/SimpleObject/ObjectClass.vb
@@ -1,56 +1,69 @@
-﻿Imports System
 Imports DevExpress.Xpo
 
 Namespace SimpleObject
+
     Public Class Parent
         Inherits XPObject
 
         Public Sub New(ByVal session As Session)
             MyBase.New(session)
         End Sub
+
         Public Overrides Sub AfterConstruction()
             MyBase.AfterConstruction()
         End Sub
+
         Private _Name As String
-        Public Property Name() As String
+
+        Public Property Name As String
             Get
                 Return _Name
             End Get
+
             Set(ByVal value As String)
                 SetPropertyValue("Name", _Name, value)
             End Set
         End Property
-        <Association("ParentChildren"), Aggregated> _
-        Public ReadOnly Property Children() As XPCollection(Of Child)
+
+        <Association("ParentChildren"), Aggregated>
+        Public ReadOnly Property Children As XPCollection(Of Child)
             Get
                 Return GetCollection(Of Child)("Children")
             End Get
         End Property
     End Class
+
     Public Class Child
         Inherits XPObject
 
         Public Sub New(ByVal session As Session)
             MyBase.New(session)
         End Sub
+
         Public Overrides Sub AfterConstruction()
             MyBase.AfterConstruction()
         End Sub
+
         Private _Name As String
-        Public Property Name() As String
+
+        Public Property Name As String
             Get
                 Return _Name
             End Get
+
             Set(ByVal value As String)
                 SetPropertyValue("Name", _Name, value)
             End Set
         End Property
+
         Private _Parent As Parent
-        <Association("ParentChildren")> _
-        Public Property Parent() As Parent
+
+        <Association("ParentChildren")>
+        Public Property Parent As Parent
             Get
                 Return _Parent
             End Get
+
             Set(ByVal value As Parent)
                 SetPropertyValue("Parent", _Parent, value)
             End Set
